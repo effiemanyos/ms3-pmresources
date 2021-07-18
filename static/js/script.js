ac0c455e235b710412a8172d3c00035689059471
Effie Manyos <fmanyos@gmail.com>
2021-07-18 12:22:41 +0000

    Add resource form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 5,
+      maxDate: new Date(), // Set maximum date to "today"
+      showClearBtn: true,
+      i18n: {
+        done:"select"
+      }
+    });
 });
 
